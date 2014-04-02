5ba2af06899d6bbdb281c07a736e756e4ba7fb0b
Chris Gianelloni <chris@continuuity.com>
2014-04-02 16:17:58 -0700

    Initial kdc recipe

---
+++ recipes/kdc.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook Name:: krb5
+# Recipe:: kdc
+#
+# Copyright 2014, Continuuity, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+node['krb5']['kdc']['packages'].each do |krb5_package|
+  package krb5_package
+end
+
+case node['platform_family']
+when 'rhel'
+  kdc_dir = "/var/kerberos/krb5kdc"
+  etc_dir = kdc_dir
+when 'debian'
+  kdc_dir = "/var/lib/krb5kdc"
+  etc_dir = "/etc/krb5kdc"
+end
+
+template "#{etc_dir}/kdc.conf" do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  variables node['krb5']['kdc_conf']
+end
