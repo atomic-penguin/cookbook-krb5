062ed4aa12840f3ad8f13f894c80f5c3fd07b1b0
Chris Gianelloni <chris@continuuity.com>
2014-04-04 10:54:23 -0700

    Version bump to 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'eric.wolfe@gmail.com'
 license          'Apache 2.0'
 description      'Installs and configures krb5 authentication'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
 
 %w(redhat centos scientific amazon ubuntu debian suse).each do |os|
   supports os
