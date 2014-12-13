d13e4104cc0f7a308c8c3799354ce7df5eccb8c3
Chris Gianelloni <chris@cask.co>
2014-12-12 16:33:44 -0800

    Attempt to make krb5_keytab idempotent

--- providers/keytab.rb
+++ providers/keytab.rb
@@ -31,36 +31,40 @@ action :create do
     kadm5 = kadm5_init(node['krb5']['admin_principal'], node['krb5']['admin_password'])
     keytab = keytab_init("FILE:#{new_resource.path}")
 
-    directory ::File.dirname(new_resource.path) do
-      owner 'root'
-      group 'root'
-      mode '0755'
-      action :create
-    end
+    unless ::File.exist?(new_resource.path)
 
-    new_resource.principals.each do |princ|
-      # kt = keytab_find_principal(keytab, princ)
-      sv = kadm5_find_principal(kadm5, princ)
-      if sv.nil?
-        Chef::Application.fatal!("Principal #{princ} not found on KDC! Perhaps, you need to create it with krb5_principal, first.")
-      # elsif kt.nil?
-      #   Chef::Log.info("Principal #{princ} found on server but missing from keytab")
-      else
-        Chef::Log.info("Principal #{princ} found on server")
+      directory ::File.dirname(new_resource.path) do
+        owner 'root'
+        group 'root'
+        mode '0755'
+        action :create
       end
-    end
 
-    execute "create-#{new_resource.path}" do
-      command "kadmin -w #{node['krb5']['admin_password']} -q 'xst -kt #{new_resource.path} #{principal_list(new_resource.principals)}'"
-      not_if "test -e #{new_resource.path}"
-      action :run
-    end
+      new_resource.principals.each do |princ|
+        # kt = keytab_find_principal(keytab, princ)
+        sv = kadm5_find_principal(kadm5, princ)
+        if sv.nil?
+          Chef::Application.fatal!("Principal #{princ} not found on KDC! Perhaps, you need to create it with krb5_principal, first.")
+        # elsif kt.nil?
+        #   Chef::Log.info("Principal #{princ} found on server but missing from keytab")
+        else
+          Chef::Log.info("Principal #{princ} found on server")
+        end
+      end
 
-    file new_resource.path do
-      owner new_resource.owner
-      group new_resource.group
-      mode  new_resource.mode
-      action :create
+      principals = principal_list(new_resource.principals)
+      execute "create-#{new_resource.path}" do
+        command "kadmin -w #{node['krb5']['admin_password']} -q 'xst -kt #{new_resource.path} #{principals}'"
+        not_if "test -e #{new_resource.path}"
+        action :run
+      end
+
+      file new_resource.path do
+        owner new_resource.owner
+        group new_resource.group
+        mode  new_resource.mode
+        action :create
+      end
     end
   ensure
     keytab.close
@@ -68,7 +72,10 @@ action :create do
 end
 
 action :delete do
-  file new_resource.path do
-    action :delete
+  if ::File.exist?(new_resource.path)
+    Chef::Log.info("Removing #{new_resource.name} keytab file")
+    file new_resource.path do
+      action :delete
+    end
   end
 end
