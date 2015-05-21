78d105f6dce462877bc31ee69b794f9c2386f6c0
Joe Rocklin <joe.rocklin@gmail.com>
2015-05-21 11:31:10 -0400

    Fix foodcritic warnings

--- test/integration/default/serverspec/pam_krb5_spec.rb
+++ test/integration/default/serverspec/pam_krb5_spec.rb
@@ -3,9 +3,7 @@ require 'serverspec'
 set :backend, :exec
 
 describe 'PAM KRB5 Authentication' do
-
   describe command('authconfig --test') do
-    its(:stdout) { should match /pam_krb5 is enabled/ }
+    its(:stdout) { should match(/pam_krb5 is enabled/) }
   end
-
 end
