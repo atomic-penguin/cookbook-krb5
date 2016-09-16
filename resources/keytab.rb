a96d8bb311a6768cbe73baa4b3ed002519845c98
Chris Gianelloni <chris@cask.co>
2016-09-16 12:54:12 -0400

    Remove extra whitespace to appease the almighty Rubocop

--- resources/keytab.rb
+++ resources/keytab.rb
@@ -21,7 +21,7 @@ actions :create, :delete
 default_action :create
 
 attribute :path,       :kind_of => String,         :name_attribute => true
-attribute :principals, :kind_of => Array,          :default => [],     :required => true
+attribute :principals, :kind_of => Array,          :default => [], :required => true
 attribute :owner,      :kind_of => String,         :default => 'root'
 attribute :group,      :kind_of => String,         :default => 'root'
 attribute :mode,       :kind_of => String,         :default => '0600'
