f1218f4bf2ea1116fb70976d0d6dc899a600bd3e
Eric Hanko <eric.hanko1@gmail.com>
2017-12-19 01:41:21 -0800

    Implement logical smoke tests
    - Using InSpec as verifier, us the os resource
    to distinguish between redhat/centos distros and debian-based

--- test/integration/default/pam_krb5_spec.rb
+++ test/integration/default/pam_krb5_spec.rb
@@ -1,5 +1,17 @@
 control 'PAM KRB5 Authentication' do
-  describe command('authconfig --test') do
-    its(:stdout) { should match(/pam_krb5 is enabled/) }
+  case os[:family]
+  when 'redhat'
+    describe command('authconfig --test') do
+      its(:stdout) { should match(/pam_krb5 is enabled/) }
+    end
+  when 'debian'
+    describe file('/etc/pam.d/common-auth') do
+      its(:content) { should match(/^auth\s+\[success=\d+\sdefault=ignore\]\s+pam_krb5.so\s+minimum_uid=\d+$/) }
+    end
+
+    describe file('/usr/share/pam-configs/krb5') do
+      it { should exist }
+      its(:content) { should match(/^Default:\syes$/) }
+    end
   end
 end
