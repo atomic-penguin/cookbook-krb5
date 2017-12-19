4a3717e19c958e14fe412afa73b2ce30920bb8b0
Eric Hanko <eric.hanko1@gmail.com>
2017-12-19 00:38:48 -0800

    Use InSpec instead of ServerSpec

--- test/integration/default/pam_krb5_spec.rb
+++ test/integration/default/pam_krb5_spec.rb
@@ -1,8 +1,4 @@
-require 'serverspec'
-
-set :backend, :exec
-
-describe 'PAM KRB5 Authentication' do
+control 'PAM KRB5 Authentication' do
   describe command('authconfig --test') do
     its(:stdout) { should match(/pam_krb5 is enabled/) }
   end
