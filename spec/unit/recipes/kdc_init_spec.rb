502f2ca82e2a6219c2f8d71d40bdde38e533ba4a
Chris Gianelloni <wolf31o2@gmail.com>
2018-12-11 13:07:24 -0500

    Split KDC DB to its own recipe

--- spec/unit/recipes/kdc_init_spec.rb
+++ spec/unit/recipes/kdc_init_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'krb5::kadmin_init' do
+describe 'krb5::kdc_init' do
   context 'on Centos 6.7 x86_64' do
     let(:chef_run) do
       ChefSpec::SoloRunner.new(platform: 'centos', version: 6.7) do |node|
@@ -10,8 +10,12 @@ describe 'krb5::kadmin_init' do
       end.converge(described_recipe)
     end
 
-    it 'executes execute[create-admin-principal] block' do
-      expect(chef_run).to run_execute('create-admin-principal')
+    it 'logs create-krb5-db to info' do
+      expect(chef_run).to write_log('create-krb5-db').with(level: :info)
+    end
+
+    it 'executes execute[create-krb5-db] block' do
+      expect(chef_run).to run_execute('create-krb5-db')
     end
   end
 end
